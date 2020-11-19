3d5ce2d8f70486b526b397796c1d90cf8ec9e8f8
ksh0rt <kshort2584@gmail.com>
2020-11-18 18:56:36 -0800

    first portfolio commit

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -16,13 +16,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'n(@$=f^x&u45h_se(w&lzqzk5%^h))x%0$ki$o73pl7-hklco*'
-
 with open('secret_settings.txt') as f:
     SECRET_KEY = f.read().strip()
 
